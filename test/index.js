db9c2f9441c1e59f2568174f0a1541e56218a492
Ben Edwards <ben.edwards@clock.co.uk>
2015-09-10 17:37:08 +0100

    > Update test script to allow for ease of introduction of column numbers

--- test/index.js
+++ test/index.js
@@ -3,22 +3,32 @@
 var fs = require('fs');
 var assert = require('assert');
 var lex = require('../');
-
 var dir = __dirname + '/cases/';
+var hasErrors = true;
+
 fs.readdirSync(dir).forEach(function (testCase) {
   if (/\.jade$/.test(testCase)) {
     console.dir(testCase);
     var expected = fs.readFileSync(dir + testCase.replace(/\.jade$/, '.expected.json'), 'utf8')
                      .split(/\n/).map(JSON.parse);
     var result = lex(fs.readFileSync(dir + testCase, 'utf8'), dir + testCase);
+    var hasErrors = false;
     fs.writeFileSync(dir + testCase.replace(/\.jade$/, '.actual.json'),
                      result.map(JSON.stringify).join('\n'));
-    assert.deepEqual(expected, result);
+
+    try {
+      assert.deepEqual(expected, result);
+    } catch (ex) {
+      console.error(ex + '\n');
+      hasErrors = true;
+    }
   }
 });
 
+if (hasErrors) throw new Error('Some test cases failed');
 
 var edir = __dirname + '/errors/';
+
 fs.readdirSync(edir).forEach(function (testCase) {
   if (/\.jade$/.test(testCase)) {
     console.dir(testCase);
